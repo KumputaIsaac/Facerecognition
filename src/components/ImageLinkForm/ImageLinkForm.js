23d23c4ebfec48008b6719b1ebcd21ce34674325
KumputaIsaac <65967090+KumputaIsaac@users.noreply.github.com>
2020-10-23 09:01:07 +0100

    add

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ export default function ImageLinkForm({onchange,onclick}) {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input placeholder='enter image link to detect face' type='search' onChange={onchange} className='f4 pa2 w-70 center'/>
+                    <input placeholder='Enter image link to detect face' type='search' onChange={onchange} className='f4 pa2 w-70 center'/>
                     <button onClick={onclick} className='w-30 grow pointer f4 link ph3 pv2 dib white bg-light-red' type="submit">Detect</button>
                 </div>
                 
